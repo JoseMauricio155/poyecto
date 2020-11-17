c7692bb352df17adaa20afe68b959e3508f9d399
JoseMauricio155 <72409650+JoseMauricio155@users.noreply.github.com>
2020-11-16 22:56:58 -0600

    Creacion del proyecto y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InventarioComponent } from './inventario/inventario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule
