8595b6a1c66b500ff6dac3fec783807a125e1fb0
JoseMauricio155 <72409650+JoseMauricio155@users.noreply.github.com>
2020-11-18 13:36:49 -0600

    agregar otro omponenete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { InventarioComponent } from './inventario/inventario.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InventarioComponent
+    InventarioComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule
